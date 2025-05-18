b9e0fd3fe3e08d032027bfe78d45b5cfa95b7515
mehranahkami <mehranahkami84@gmail.com>
2025-05-18 21:26:41 +0330

    Initial commit

---
+++ database/migrations/2025_05_17_203050_create_company_lists_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('company_lists', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('description');
+            $table->string('website');
+            $table->string('category');
+            $table->string('logo');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('company_lists');
+    }
+};
